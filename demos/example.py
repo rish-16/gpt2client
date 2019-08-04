c9d6268ae5b191153ff8ee9c141e3a7370079c94
rish-16 <mail.rishabh.anand@gmail.com>
2019-08-04 18:23:16 +0800

    Added generative model

--- demos/example.py
+++ demos/example.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+
 from gpt2client import GPT2Client
 
-gpt2 = GPT2Client()
+gpt2 = GPT2Client(model_name='117M')
 gpt2.download_model()
+
+text = gpt2.generate(display=False)
+print (text)
