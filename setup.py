d81d7295ca4fa824f208af0248dedc7bc8490af3
rish-16 <mail.rishabh.anand@gmail.com>
2019-08-09 14:52:13 +0800

    Updated PyPi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
 	name = 'gpt2_client',
 	packages = ['gpt2_client'],
-	version = '1.9.3',
+	version = '1.9.4',
 	license='MIT',
 	description = 'Easy-to-use Wrapper for the GPT-2 117M and 345M Transformer Models',
 	long_description = long_description,
@@ -17,7 +17,7 @@ setup(
 	author = 'Rishabh Anand',
 	author_email = 'mail.rishabh.anand@gmail.com',
 	url = 'https://github.com/rish-16/gpt2client',
-	download_url = 'https://github.com/rish-16/gpt2client/archive/1.9.3.tar.gz',
+	download_url = 'https://github.com/rish-16/gpt2client/archive/1.9.4.tar.gz',
 	keywords = ['gpt-2', 'wrapper', 'transformer', 'machine learning', 'openai', 'text generation'],
 	install_requires=[
 			'numpy',
